766550f59df741e29300daced5a061907096f062
myName005 <eer37779@gmail.com>
2017-09-17 18:33:07 +0100

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
   <div id="app">
-  
+    <div class="container" >
+      <navbar></navbar>  
+      <div class="columns">
+        <div class="column is-8">
+          
+        </div>
+        <div class="column is-4">
+            
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import navbar from './components/navbar.vue'
+
+
 export default {
-  name: 'app'
+  name: 'app',
+  components:{
+    'navbar':navbar
+  }
 }
 </script>
+
+<style lang="scss">
+@import './../node_modules/bulma/bulma.sass';
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,9 @@
+<template>
+	<nav class="navbar">
+		<div class="navbar-brand">
+			<a class="navbar-item" href="#">
+				<h3 class="title is-3">GraphyFun</h3>
+			</a>
+		</div>
+	</nav>
+</template>
